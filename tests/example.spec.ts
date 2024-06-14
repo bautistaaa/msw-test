dbd946fd8a5e560c9414944577f64ec837659cd1
Artem Zakharchenko <kettanaito@gmail.com>
2024-06-14 11:22:08 +0200

    fix playwright test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,24 +1,24 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test'
 
-test("has hello", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+test('has hello', async ({ page }) => {
+  await page.goto('http://localhost:3000')
 
-  await expect(page.getByText("Hello", { exact: true })).toBeVisible();
-});
+  await expect(page.getByText('Hello', { exact: true })).toBeVisible()
+})
 
-test("has world", async ({ page }) => {
+test('has world', async ({ page }) => {
+  await page.goto('http://localhost:3000')
+
+  await page.waitForFunction(() => window.msw)
+
   await page.evaluate(() => {
-    const { worker, http, HttpResponse } = window.msw;
+    const { worker, http, HttpResponse } = window.msw
     worker.use(
-      ...[
-        http.get("/greeting", () => {
-          return HttpResponse.json("World");
-        }),
-      ]
-    );
-  });
+      http.get('/greeting', () => {
+        return HttpResponse.json('World')
+      })
+    )
+  })
 
-  await page.goto("http://localhost:3000");
-
-  await expect(page.getByText("World", { exact: true })).toBeVisible();
-});
+  await expect(page.getByText('World', { exact: true })).toBeVisible()
+})
