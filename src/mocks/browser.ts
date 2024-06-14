fd1fad82563ba537e42fd1b9c33319e2c50dd77d
Chris Bautista <chrisbautista@netflix.com>
2024-06-14 09:16:13 -0700

    import mocks breaks

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,11 +1,12 @@
 import { setupWorker } from "msw/browser";
-import { http, HttpResponse } from "msw";
+import { graphql, http, HttpResponse } from "msw";
 import { handlers } from "./handlers";
 
 export const worker = setupWorker(...handlers);
 
 window.msw = {
   worker,
+  graphql,
   http,
   HttpResponse,
 };
